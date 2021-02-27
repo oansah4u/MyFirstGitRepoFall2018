d3f0a84ab535f39bb80cd4f62db47f121639968f
oansah4u <oansah4u@yahoo.com>
2021-02-27 16:54:46 -0500

    adding a file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("Good Evening");
+
+        int a = 20;
+        int b = 30;
+        System.out.println(a+b);
     }
 }
